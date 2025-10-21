1e137b6bd6e6b7a7fe1c3d95e68cb79b833b79a9
FKBGRENSHE <fkbgrenshe@163.com>
2025-10-21 16:24:24 +0800

    update

---
+++ src/main/java/com/JUC/AQS/ReentrantLock.java
@@ -0,0 +1,9 @@
+package com.JUC.AQS;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2025-10-21
+ * @Description: AQS框架-可重入锁
+ */
+public class ReentrantLock {
+}
--- src/main/java/com/JUC/ConcurrentContainer/BlockingQueue/BlockingQueueAnalysis.java
+++ src/main/java/com/JUC/ConcurrentContainer/BlockingQueue/BlockingQueueAnalysis.java
@@ -1,9 +1,19 @@
 package com.JUC.ConcurrentContainer.BlockingQueue;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * @Author: Bingyu Chen
  * @CreateTime: 2025-10-21
  * @Description: 阻塞队列接口分析
  */
 public class BlockingQueueAnalysis {
+
+    public static void main(String[] args) {
+
+        BlockingQueue<Object> testQueue = new ArrayBlockingQueue<>(15);
+    }
+
 }
