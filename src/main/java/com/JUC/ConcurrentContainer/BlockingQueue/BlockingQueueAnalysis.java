d002f731e1d56512b37b58946d65eb8ccbd4e668
FKBGRENSHE <fkbgrenshe@163.com>
2025-10-21 16:16:55 +0800

    update

---
+++ src/main/java/com/JUC/ConcurrentContainer/BlockingQueue/BlockingQueueAnalysis.java
@@ -0,0 +1,9 @@
+package com.JUC.ConcurrentContainer.BlockingQueue;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2025-10-21
+ * @Description: 阻塞队列接口分析
+ */
+public class BlockingQueueAnalysis {
+}
--- src/main/java/com/SchedulingAlogrithms/PageReplacementAlgorithms/LFU.java
+++ src/main/java/com/SchedulingAlogrithms/PageReplacementAlgorithms/LFU.java
@@ -5,5 +5,34 @@ package com.SchedulingAlogrithms.PageReplacementAlgorithms;
  * @CreateTime: 2025-10-19
  * @Description: 最不经常使用页面置换算法
  */
-public class LFU {
+public class LFU implements PageRepalcementPolicy{
+    @Override
+    public void accessPage(int pageId) {
+
+    }
+
+    @Override
+    public void removePage(int pageId) {
+
+    }
+
+    @Override
+    public Page getVictim() {
+        return null;
+    }
+
+    @Override
+    public boolean contains(int pageId) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public String currentState() {
+        return "";
+    }
 }
