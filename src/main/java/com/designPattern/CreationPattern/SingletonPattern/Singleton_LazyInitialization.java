805b741052a2cfc8a30f1427e93d4cdfbf513bee
FKBGRENSHE <fkbgrenshe@163.com>
2026-02-08 16:32:27 +0800

    update Singleton_LazyInitialization

---
+++ src/main/java/com/designPattern/CreationPattern/SingletonPattern/Singleton_LazyInitialization.java
@@ -0,0 +1,9 @@
+package com.designPattern.CreationPattern.SingletonPattern;
+
+/**
+ * @Author: Bingyu Chen
+ * @CreateTime: 2026-02-08
+ * @Description: 单例-懒汉式
+ */
+public class Singleton_LazyInitialization {
+}
